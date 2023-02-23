189b924ebee0c6a75c18715325732ce1f28dbed5
Muhammad Ijaz <ijazlovely00786@gmail.com>
2023-02-23 09:24:48 +0500

    removed scss file also included a todo route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './scss/global.css';
+// import './scss/global.scss';
 import Signup from './pages/auth/signup/Signup';
 import Login from './pages/auth/login/Login';
 import {
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import ForgetPassword from './pages/auth/forget-password/ForgetPassword';
 import ResetPassword from './pages/auth/reset-password/ResetPassword';
+import Todo from './components/todo/Todo';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Route path="/signup" element={<Signup />} />
       <Route path="/forget-password" element={<ForgetPassword />} />
       <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/todo" element={<Todo />} />
     </Routes>
   );
 }
