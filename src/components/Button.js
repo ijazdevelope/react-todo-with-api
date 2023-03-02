cd588ecee26b4feef035b954e90919513cabb6de
Muhammad Ijaz <ijazlovely00786@gmail.com>
2023-03-02 19:02:44 +0500

    pass onclik function as an props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Button = ({className, value, type}) => {
+const Button = ({className, value, type, onClick}) => {
     return (
         <>
-            <button className={className} type={type} >{value}</button>
+            <button onClick={onClick && onClick} className={className} type={type} >{value}</button>
         </>
     )
 }
