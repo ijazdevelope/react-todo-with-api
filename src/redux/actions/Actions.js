7b7bad36915fda97f741836ae2ffcd9e183ebfa7
Muhammad Ijaz <ijazlovely00786@gmail.com>
2023-04-01 09:33:48 +0500

    added new action fuction for add todos

--- src/redux/actions/Actions.js
+++ src/redux/actions/Actions.js
@@ -1,8 +1,18 @@
-import { INCREMENT } from '../constants/Constants'
+import { actionTypes, INCREMENT } from '../constants/Constants'
 
 export const Action = (payload) => {
     return {
         type: INCREMENT,
+        payload
+    }
+}
+
+export const addTodo = (payload) => {
+    localStorage.setItem('todo', JSON.stringify(payload));
+
+    console.log(payload, 'object in actions...')
+    return {
+        type: actionTypes.ADD_TODO,
         payload
     }
 }
